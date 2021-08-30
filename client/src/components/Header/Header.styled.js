2bb760257d151263349384a8a97516993c71f630
jrozal <68182930+jrozal@users.noreply.github.com>
2021-08-29 22:43:42 -0700

    Add responsive styling to header

--- client/src/components/Header/Header.styled.js
+++ client/src/components/Header/Header.styled.js
@@ -2,31 +2,69 @@ import styled from 'styled-components';
 
 const Container = styled.header`
   display: flex;
-  padding: 3.25rem;
+  padding: 3.25rem 8rem;
   justify-content: center;
   align-items: center;
+
+  @media only screen and (max-width: 735px) {
+    flex-direction: column-reverse;
+    padding: 1rem;
+  }
 `;
 
 const Content = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 60rem;
-  min-width: 25rem;
-  margin: 5rem;
+  max-width: 40rem;
+  margin-right: 3rem;
+
+  @media only screen and (max-width: 735px) {
+    margin: 0;
+  }
 `;
 
 const Media = styled.div`
-  min-width: 400px;
+  width: 400px;
+  min-width: 300px;
+  height: auto;
+
+  &:after {
+    content: '';
+    display: block;
+    margin: auto;
+    bottom: -30px;
+    left: 50%;
+    height: 8px;
+    width: 60%;
+    box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.4);
+    border-radius: 50%;
+    background-color: rgba(0, 0, 0, 0.2);
+    filter: blur(2px);
+  }
+
+  @media only screen and (max-width: 735px) {
+    max-width: 200px;
+  }
 `;
 
 const H1 = styled.h1`
-  font-size: 3rem;
+  font-size: 5rem;
   text-transform: uppercase;
+  margin: 2rem 0;
+
+  @media only screen and (max-width: 735px) {
+    margin: 1rem 0;
+    font-size: 2.5rem;
+  }
 `;
 
 const Text = styled.p`
-  font-size: 1.5rem;
-  color: #7A7A7A
+  font-size: 1.3rem;
+  color: #7A7A7A;
+
+  @media only screen and (max-width: 735px) {
+
+  }
 `;
 
 export {
