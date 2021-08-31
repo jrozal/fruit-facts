20b909e53b84883ffcc492555ebb7b8250a636fd
jrozal <68182930+jrozal@users.noreply.github.com>
2021-08-30 23:04:04 -0700

    [WIP] Pop up modal

--- client/src/components/FruitGallery/index.js
+++ client/src/components/FruitGallery/index.js
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import { Gallery, Card } from './FruitGallery.styled';
+import Modal from '../Modal';
+import useModal from '../../useModal';
 
 const FruitGallery = (props) => {
+  const { isOpen, toggle } = useModal();
+  const [modalData, setModalData] = useState({});
+
+  const handleClick = (data) => () => {
+    setModalData({ test: data });
+    toggle();
+  };
 
   const photos = props.photos.map((photo, i) => {
-    return <Card key={i} img={photo}></Card>
-  })
+    return <Card key={i} img={photo} onClick={handleClick('AHH')}></Card>
+  });
 
   return (
-    <Gallery>{photos}</Gallery>
-  )
+    <div>
+      <Modal isOpen={isOpen} close={toggle} data={modalData} />
+      <Gallery>{photos}</Gallery>
+    </div>
+  );
 };
 
 export default FruitGallery;
