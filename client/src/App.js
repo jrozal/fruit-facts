43947fdcbb072a69e695948a3e4a825ec8a838da
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-01 23:22:59 -0700

    Remove console.log for prod

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ const App = () => {
       const response = await axios.get('/data');
       setLoading(false);
       setData(response.data);
-      console.log(response.data)
     }
 
     catch (error) {
