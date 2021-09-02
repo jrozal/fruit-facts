aa9f2011c09a73b08de154d07921a3625a8e18e9
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-01 20:12:25 -0700

    Add photo data

---
+++ server/api/photos.js
@@ -0,0 +1,19 @@
+const photos = {
+  "apple": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630479237/apple_dtbs2s.jpg",
+  "apricot": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630479289/apricot_l784mk.jpg",
+  "banana": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630479297/banana_jvoixn.jpg",
+  "blueberry": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630479306/blueberry_rt9snf.jpg",
+  "cherry": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630479313/cherry_gfc0ug.jpg",
+  "guava": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630479322/guava_hgxhij.jpg",
+  "lemon": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630479330/lemon_nk91ea.jpg",
+  "mango": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630479345/mango_oeh4t7.jpg",
+  "orange": "https://res.cloudinary.com/db8r02snr/image/upload/h_750,c_scale/v1630479354/orange_rvrudx.jpg",
+  "pear": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630479367/pear_bshqhe.jpg",
+  "pineapple": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630479379/pineapple_uhrwtr.jpg",
+  "raspberry": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630479924/raspberry_bw4jbr.jpg",
+  "strawberry": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630479945/strawberry_t4mmli.jpg",
+  "tomato": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630480111/tomato_clgdag.jpg",
+  "watermelon": "https://res.cloudinary.com/db8r02snr/image/upload/w_750,c_scale/v1630479994/watermelon_s5x84k.png"
+};
+
+module.exports = photos;
