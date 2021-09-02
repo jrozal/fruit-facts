da2f6c2c0f3f510688961dd947210d89051569b6
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-01 21:46:03 -0700

    Add loading circle

---
+++ client/src/components/Loading/Loading.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export { LoadingContainer };
---
+++ client/src/components/Loading/index.js
@@ -0,0 +1,9 @@
+import { LoadingContainer } from './Loading.styled'
+
+const Loader = () => (
+  <LoadingContainer>
+    <img src="https://res.cloudinary.com/db8r02snr/image/upload/v1630557392/loading_uvmgo1.gif" alt=""></img>
+  </LoadingContainer>
+);
+
+export default Loader;
