6cd807d4bf5d59d08b056cada22678d7a69cea36
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-01 23:22:04 -0700

    Prepare server for deployment

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3001;
+const port = process.env.PORT || 3000;
 const getFruitsAndPhotos = require('./controller');
 
 app.use(cors({ origin: '*' }));
 
+const publicPath = path.join(__dirname, '..', '/client/build');
+
+app.use(express.static(publicPath));
+
 app.get('/data', (req, res) => {
   getFruitsAndPhotos(req, res);
+});
+
+// send to homepage for non-existant resource
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
