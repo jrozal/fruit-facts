099b748d5b5e2e641490dc3ac0324ee660cf2d23
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-01 21:47:57 -0700

    Update App components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import FruitGallery from './components/FruitGallery';
+import Header from './components/Header';
+import Loading from './components/Loading';
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/data');
+      setLoading(false);
+      setData(response.data);
+      console.log(response.data)
+    }
+
+    catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-      Hello World
+      <Header />
+      {loading && <Loading/>}
+      {!loading && <FruitGallery data={data}/>}
     </div>
   );
 }
