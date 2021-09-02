1ea8a47cea98b4b8f65741445e268720456043eb
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-01 23:18:23 -0700

    Update get req url for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:3001/data');
+      const response = await axios.get('/data');
       setLoading(false);
       setData(response.data);
       console.log(response.data)
